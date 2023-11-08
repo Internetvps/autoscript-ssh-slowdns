e2ea4822d011148a08999a4927d8808ac6ea34f7
Internetvps <89685787+Internetvps@users.noreply.github.com>
2023-11-08 13:59:14 +0800

    Update about.sh

--- system/about.sh
+++ system/about.sh
@@ -37,9 +37,9 @@ gl='\e[32;1m'
 clear
 echo ""
 figlet " SCRIPT" | lolcat
-figlet " BY PAKYA" | lolcat
+figlet " BY DRGVPN" | lolcat
 echo -e "\e[32;1m.-----------------------------------------------.\e[0m"
-echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT VPS\e[0m                 \e[32;1m|\e[0m"
+echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT DRGVPN VPS\e[0m                 \e[32;1m|\e[0m"
 echo -e "\e[32;1m'-----------------------------------------------'\e[0m"
 echo -e "           \e[031;1m> Premium Script By DRGVPN <\e[0m" | lolcat
 echo -e " \e[0;32m_______________________________________________\e[0m"
@@ -49,10 +49,10 @@ echo -e "\e[0;32m|               \e[1;35mI HOPE YOU HAPPY                \e[0;32
 echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
 echo -e "\e[0;32m|                   \e[1;36mTHANKS TO                   \e[0;32m|\e[0m"
 echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
-echo -e "\e[0;32m|                   \e[1;35mDRGVPNSERVER                 \e[0;32m|\e[0m"
+echo -e "\e[0;32m|                   \e[1;35mDRGVPN TEAM                 \e[0;32m|\e[0m"
 echo -e "\e[0;32m|                   \e[1;35mALLAH SWT                   \e[0;32m|\e[0m"
 echo -e "\e[0;32m|                   \e[1;35mMy Family                   \e[0;32m|\e[0m"
 echo -e "\e[0;32m|                    \e[1;35mGoogle                     \e[0;32m|\e[0m"
 echo -e "\e[0;32m|_______________________________________________|\e[0m"
-echo -e "\e[0;32m|_______________\e[36;1mTHANKYOU SUPPORT\e[0m\e[0;32m________________|\e[0m"
+echo -e "\e[0;32m|_______________\e[36;1mTHANKYOU SUPPORT DRGVPN\e[0m\e[0;32m________________|\e[0m"
 echo -e "               \e[0;32m'----------------'\e[0m"
