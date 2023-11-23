a9480fc76dfcf33974a482f00218c886b141972f
Internetvps <89685787+Internetvps@users.noreply.github.com>
2023-11-23 22:41:50 +0800

    Update addip.sh

--- menu/addip.sh
+++ menu/addip.sh
@@ -1,33 +1,4 @@
 #!/bin/bash
-#wget https://github.com/${GitUser}/
-GitUser="Internetvps"
-#IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
-echo -e "\e[32mloading...\e[0m"
-clear
-# Valid Script
-VALIDITY () {
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-VALIDITY
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
-fi
-echo -e "\e[32mloading...\e[0m"
-clear
 # Color Validation
 DF='\e[39m'
 Bold='\e[1m'
@@ -49,6 +20,14 @@ ORANGE='\033[0;33m'
 CYAN='\e[36m'
 LIGHT='\033[0;37m'
 tokengit=$(cat /etc/adminip/access.conf)
+MYIP=$(wget -qO- ipinfo.io/ip);
+adminip=$( curl -sS https://raw.githubusercontent.com/Internetvps/allow/main/accessmenu | grep $MYIP )
+if [ $adminip = $MYIP ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+rm -rf addip.sh
+clear
+
 setadmin() {
 #isi link git
 linkinstall="https://raw.githubusercontent.com/namagit/git-repo/main"
@@ -66,7 +45,7 @@ cat << EOF >> /etc/crontab
 EOF
 rm -f /root/.bash_history
 sleep 2
-echo " Setelah Selesai type menu-admin untuk access"
+echo " Setelah Selesai type addip untuk access"
 }
 
 add-ip() {
@@ -114,7 +93,7 @@ fi
 
 daftarip=$(cat /root/data)
 rm -rf /root/allow
-git config --global user.email "alifgaming9546@gmail.com"
+git config --global user.email "aquagurl2601@gmail.com"
 git config --global user.name "Internetvps"
 git clone https://github.com/Internetvps/allow.git
 mkdir /root/allow
@@ -134,8 +113,8 @@ echo -e "IPVPS Registration Completed"
 sleep 1
 links1="apt-get update && apt-get upgrade -y && update-grub && sleep 2 && reboot"
 links2="apt-get update && apt-get upgrade -y && apt dist-upgrade -y && update-grub && sleep 2 && reboot"
-links3="sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl && wget https://raw.githubusercontent.com/Internetvps/autoscript-ssh-slowdns/main/setup.sh && chmod +x setup.sh && sed -i -e 's/\r$//' setup.sh && screen -S setup ./setup.sh"
-clear
+links3="sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl && wget https://raw.githubusercontent.com/NikmatSenip/hasilsenip/main/setup.sh && chmod +x setup.sh && sed -i -e 's/\r$//' setup.sh && screen -S setup ./setup.sh"
+
 echo -e "\033[0;34m----------------------------------------\033[0m"
 echo "  Client IP VPS Add Successfully"
 echo -e "\033[0;34m----------------------------------------\033[0m"
@@ -144,7 +123,7 @@ echo "  Order ID      : $id"
 echo "  Register Date : $hariini"
 echo "  Expired Date  : $exp"
 echo "  Client Name   : $client"
-echo "  Script Ver    : Multiport_drgvpn"
+echo "  Script Ver    : AQUAGURL_MULTIPORT"
 echo -e "\033[0;34m----------------------------------------\033[0m"
 echo " Update & Upgrade First Your VPS for Debian 10 & 11: "
 echo ""
@@ -159,7 +138,7 @@ echo ""
 echo -e '' ${links3}''
 echo ""
 echo -e "\033[0;34m----------------------------------------\033[0m"
-echo "              Nota BY DRGVPN"
+echo "                          NOTA"
 echo -e "\033[0;34m----------------------------------------\033[0m"
 echo "  siapkan email cloud flare untuk cert xray  "
 echo "  pastikan domain dah siap2 pointing di CF ya sblm install  "
@@ -183,7 +162,7 @@ clear
 rm -rf /root/allow
 rm -rf /root/data
 rm -rf /root/ipvps.conf
-git config --global user.email "alifgaming9546@gmail.com"
+git config --global user.email "aquagurl2601@gmail.com"
 git config --global user.name "Internetvps"
 git clone https://github.com/Internetvps/allow.git
 mkdir /root/allow
@@ -238,7 +217,7 @@ exit 0
 fi
 clear
 rm -rf /root/allow
-git config --global user.email "alifgaming9546@gmail.com"
+git config --global user.email "aquagurl2601@gmail.com"
 git config --global user.name "Internetvps"
 git clone https://github.com/Internetvps/allow.git
   mkdir /root/allow
@@ -317,7 +296,7 @@ clear
 rm -rf /root/allow
 rm -rf /root/data
 rm -rf /root/ipvps.conf
-git config --global user.email "alifgaming9546@gmail.com"
+git config --global user.email "aquagurl2601@gmail.com"
 git config --global user.name "Internetvps"
 git clone https://github.com/Internetvps/allow.git
 cd /root/allow/
@@ -344,7 +323,7 @@ addip
 
 clear
 echo -e "\033[0;34m----------------------------------------\033[0m"
-echo -e "\E[44;1;39m    MENU ADD IP MULTIPORT WS DRGVPN  \E[0m"
+echo -e "\E[44;1;39m    MENU ADD IP MULTIPORT WS  \E[0m"
 echo -e "\033[0;34m----------------------------------------\033[0m"
 echo ""
 echo -e " [\e[36m 01 \e[0m] Add IP"
@@ -356,7 +335,7 @@ echo ""
 echo -e "Press x or [ Ctrl+C ]   To-Exit"
 echo -e ""
 echo -e "\033[0;34m----------------------------------------\033[0m"
-echo -e "\E[44;1;39m      SCRIPT MULTIPORT WS DRGVPN     \E[0m"
+echo -e "\E[44;1;39m      SCRIPT MULTIPORT WS \E[0m"
 echo -e "\033[0;34m----------------------------------------\033[0m"
 echo ""
 read -p " Select menu : " opt
